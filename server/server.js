3917810e124e91ddd7ffa996442fd2fd3f6da93b
Marcus Nygren <marcusnygren.mn@gmail.com>
2014-03-15 15:29:19 +0100

    Changed server port

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ var app = express()
 , server = require('http').createServer(app)
 , io = io.listen(server);
 
-server.listen(8080);
+server.listen(8081);
 
 io.sockets.on('connection', function (socket) {
     socket.emit('news', { hello: 'world' });
