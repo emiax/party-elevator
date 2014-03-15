dfb103d3a0e4374cde1cb38d0450c4f018557529
emiax <mail@emilaxelsson.se>
2014-03-15 12:00:49 +0100

    sample server

---
+++ server/server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var io = require('socket.io');
+
+
+var app = express()
+, server = require('http').createServer(app)
+, io = io.listen(server);
+
+server.listen(8080);
+
+io.sockets.on('connection', function (socket) {
+    socket.emit('news', { hello: 'world' });
+    socket.on('getAll', function (data) {
+        console.log(data);
+        socket.emit('test');
+    });
+});
