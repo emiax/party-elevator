62ed035fe626e15de593492e93100474fb5d2fb5
emiax <mail@emilaxelsson.se>
2014-03-19 22:43:11 +0100

    server conforms to new map loading structure

--- server/server.js
+++ server/server.js
@@ -14,36 +14,38 @@ server.listen(8081);
 var defaultState = new State();
 var sockets = {};
 
-io.sockets.on('connection', function (socket) {
+Pathfinder.loadMap(function () {
+    io.sockets.on('connection', function (socket) {
 
-    var attendee = new Attendee(defaultState);
-    party.addAttendee(attendee);
-    sockets[attendee.id()] = socket;
+        var attendee = new Attendee(defaultState);
+        party.addAttendee(attendee);
+        sockets[attendee.id()] = socket;
 
-    //    socket.emit('allStates', party.allStates());
-    socket.on('intention', function (data) {
-        // handle input.
-        var x = data.x;
-        var y = data.y;
-        var level;
-        switch (data.level) {
-        case 'top': level = State.LevelEnum.TOP;
-        case 'elevator': level = State.LevelEnum.ELEVATOR;
-        default: level = State.LevelEnum.GROUND;
-        }
+        //    socket.emit('allStates', party.allStates());
+        socket.on('intention', function (data) {
+            // handle input.
+            var x = data.x;
+            var y = data.y;
+            var level;
+            switch (data.level) {
+            case 'top': level = State.LevelEnum.TOP;
+            case 'elevator': level = State.LevelEnum.ELEVATOR;
+            default: level = State.LevelEnum.GROUND;
+            }
 
-        // do stuff.
-        party.setIntention(attendee, new State({
-            x: x,
-            y: y,
-            level: level
-        }));
+            // do stuff.
+            party.setIntention(attendee, new State({
+                x: x,
+                y: y,
+                level: level
+            }));
 
-        // send updates to clients.
-        Object.keys(sockets).forEach(function (attendeeId) {
-            var outputSocket = sockets[attendeeId];
-            outputSocket.emit('attendeeKeyframes', attendee.toClientFormat());
-        })
+            // send updates to clients.
+            Object.keys(sockets).forEach(function (attendeeId) {
+                var outputSocket = sockets[attendeeId];
+                outputSocket.emit('attendeeKeyframes', attendee.toClientFormat());
+            })
+        });
     });
 });
 
